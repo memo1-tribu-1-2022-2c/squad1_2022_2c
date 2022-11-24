6c0cbd4a19736b6ca999197131877dd5311e1ddd
BautistaXifro <bauxifro@gmail.com>
2022-11-23 22:33:22 -0300

    tickets con toda la bd completa

--- services/ticket.py
+++ services/ticket.py
@@ -1,32 +1,42 @@
 from config import db
+import requests
 
 class TicketService():
 
     def __init__(self):
         self.cursor = db.cursor()
 
-    def __del__(self):
-        self.cursor.close()
-
     def get_all_tickets(self):
         
         self.cursor.execute("SELECT * FROM tickets;")
         tickets = self.cursor.fetchall()
-        values = [{'ticket_id': value[0], 'ticket_title': value[1]} for value in tickets]
+        values = [{'ticket_id': value[0], 'ticket_title': value[2]} for value in tickets]
         return values
 
     def get_ticket(self,ticket_id):
-        get_query = """SELECT * FROM tickets WHERE id = %i"""
-        self.cursor.execute(get_query, ticket_id)
+        get_query = """SELECT * FROM tickets WHERE id = %s"""
+        self.cursor.execute(get_query, (ticket_id,))
         ticket = self.cursor.fetchone()
-        return ticket
+        return {'ticket_id': ticket[0], 'ticket_title': ticket[2]}
 
     def create_ticket(self, kwargs):
-        insert_query = """INSERT INTO tickets (id, title) VALUES (%s, %s);"""
-        record_to_insert = (kwargs['ticket'], kwargs['ticket_title'])
-        self.cursor.execute(insert_query, record_to_insert)
-        db.commit()
-        return self.cursor.rowcount
+        clients = requests\
+        .get("https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/clientes-psa/1.0.0/m/api/clientes")\
+        .json()
+        for value in clients:
+            print(kwargs['ticket_client'] == value['razon social'])
+        if({kwargs['ticket_client'] == value['razon social']} for value in clients):
+            insert_query = """INSERT INTO tickets (start_dt, title, client, proyect_id, description, state, person_in_charge, end_dt) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"""
+            record_to_insert = (kwargs['ticket_start_dt'], kwargs['ticket_title'], kwargs['ticket_client'],\
+                int(kwargs['ticket_proyect_id']), kwargs['ticket_description'], kwargs['ticket_state'],\
+                kwargs['ticket_person_in_charge'], kwargs['ticket_end_dt'])
+            self.cursor.execute(insert_query, record_to_insert)
+            db.commit()
+            self.cursor.execute("SELECT MAX(id) FROM tickets;")
+            result = self.cursor.fetchone()
+            return result[0]
+        else:
+            return -1
 
     def update_ticket(self, kwargs):
         update_query = """UPDATE tickets SET title = %s WHERE id = %s"""
